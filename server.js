b189c5a58dbb547d72853196ae278904e0f92b90
Bharath Kesineni <bharath.kesineni@brainvire.com>
2024-08-06 15:09:25 +0530

    created a server in express js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.use("/", (req, res) => {
+  res.send("Welcome to my API!");
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
